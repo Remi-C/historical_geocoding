cd8c87b729bc11f2036251dfcef6dd9728a6b965
Remi-C <remi.cura@gmail.com>
2016-10-19 13:27:30 +0200

    new script to load current IGN data

---
+++ scripts/importing_ign/loading_ign.sql
@@ -0,0 +1,15 @@
+﻿--------------------------------
+-- Rémi Cura, 2016
+-- projet geohistorical data
+-- 
+--------------------------------
+-- import and normalise of current data
+-- BDAdress for adress number, BDTopo for street name
+--------------------------------
+
+    CREATE SCHEMA IF NOT EXISTS ign_paris;
+    SET search_path to ign_paris, historical_geocoding, geohistorical_object, public; 
+
+
+-- importing the two files
+	
